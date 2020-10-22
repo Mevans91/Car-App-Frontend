dc393c91385b0dc3dd9f639ee990c8fa5bc436f8
Mevans91 <dragonscale_1@hotmail.com>
2020-10-22 16:49:41 -0500

    home fixed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { User } from '../_models/user';
+import { UserService } from '../_services/user.service';
 
 
 @Component({
@@ -8,11 +12,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  currentUser: User
+  private subs = new Subscription
   constructor(
+    private userService: UserService,
+    public router: Router
   ) { }
 
   ngOnInit(): void {
-
+    this.subscribeToCurrentUser()
   }
+
+  subscribeToCurrentUser() {
+    this.subs.add( //creates a new subscription
+     this.userService.currentUser.subscribe(user => { //creates the subscription to our currentUser
+       if (user) { //checks to see if the logged in user exists
+         this.currentUser = user //sets our currentUser value in our component
+       } else { //if the current user doesnt exist
+         this.currentUser = null //we set the current user as null
+       }
+     })
+   )
+ }
 
 }
